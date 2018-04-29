074ac7f5621977d84d0588afed143ba5595fe4de
Reenakoudi <kreena92@gmail.com>
2018-04-29 18:13:49 +0100

    Refactoring into MVC by creating Peep Model

--- app.rb
+++ app.rb
@@ -2,12 +2,8 @@ require 'sinatra/base'
 
 class Chitter < Sinatra::Base
   get '/' do
-    peeps = ['My 1st peep',
-    'My 2nd peep',
-    'My 3rd peep']
-    peeps.join
-
-    end
+    @peeps = Peep.all
+  end
 
   run! if app_file == $0
 end
---
+++ lib/peep.rb
@@ -0,0 +1,7 @@
+class Peep
+  def self.all
+    ['My 1st peep',
+     'My 2nd peep',
+     'My 3rd peep']
+  end
+end
